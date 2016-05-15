84b7637d497f8cd6670ac86d4ba27c83aa266425
stoiko <stoiko.todorov@gmail.com>
2016-05-16 00:12:23 +0300

    added sdl building script

---
+++ sdl_build_debug_on_cygwin_with_mingw.sh
@@ -0,0 +1,2 @@
+CFLAGS="-O0 -g" LDFLAGS="-L/usr/x86_64-w64-mingw32/lib" CC="/usr/bin/x86_64-w64-mingw32-gcc.exe" CPP="/usr/bin/x86_64-w64-mingw32-cpp" CXX="/usr/bin/x86_64-w64-mingw32-g++" ./configure --disable-render-d3d
+make -j4
