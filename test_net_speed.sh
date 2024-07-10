5dbd92de056fbb7a0896abda62374c6a1433ddfa
zloedi <stoiko.todorov@gmail.com>
2024-07-10 10:10:07 +0300

    vimrc update

--- test_net_speed.sh
+++ test_net_speed.sh
@@ -1,1 +1,1 @@
-curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python2.7 -
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
